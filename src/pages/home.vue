335a70f74f562fcad37df7780e1e2a34b31cac82
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-09-28 01:37:09 +0200

    filtri

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,27 +9,64 @@ export default{
 },
 data(){
     return{
-      result : []
+      result : [],
+      selectedTag:[],
+      tags: [],
+      numPerPage: null
      
     }
   },
   mounted(){
-    this.chiamataApi();
-    
+    //this.chiamataApi();
+    this.getTags(this.numPerPage);
   },
+   watch: {
+     selectedTag:{
+       handler:'getTags', 
+       deep:true
+     }
+   },
   methods: {
     chiamataApi(){
       axios.get('http://127.0.0.1:8000/api/image')
       .then((res) => {
-        this.result = res.data.image;
+        this.result = res.data;
         console.log(this.result)
     });
+    },
+    getTags(numPerPage){
+      const params = {page: numPerPage}
+      if(this.selectedTag.length > 0){
+        params.tags_id = this.selectedTag.join(',')
+        console.log(params.tags_id);
+      }
+      if(numPerPage >= 3){
+        params.num_page = this.numPerPage;
+        console.log(params.num_page);
+      }
+      axios.get('http://127.0.0.1:8000/api/image', {params})
+      .then((res) => {
+        this.result = res.data.image.data;
+        this.currentPage = res.data.image.current_page;
+        this.lastPage = res.data.image.last_page;
+        this.tags = res.data.tag;
+      })
     }
   }
 }  
 </script>
 
 <template>
+    <div>
+      <div v-for="(elem, index) in this.tags" :key="index">
+        <input  type="checkbox" :value="elem.id" v-model="selectedTag">
+        <span>{{ elem.tag }}</span>
+      </div>
+      <div>
+        <input type="number" placeholder="inserisci quante foto visualizzare" v-model="numPerPage" min="3">
+        <button class="btn btn-primary" @click="this.getTags(this.numPerPage)">Submit</button>
+      </div>
+    </div>
     <div class="container">
         <div class="row">
             <div id="card" class="col-12 col-md-6 col-lg-4"  v-for="(elem, index) in this.result" :key="index">
@@ -39,7 +76,25 @@ data(){
             </div>
         </div>
     </div>
-
+    <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a @click.prevent="getTags(currentPage - 1) " class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true" >&laquo;</span>
+        </a>
+      </li>
+      <li v-for="(elem, index) in lastPage" :key="index" :class="(currentPage === elem) ? 'active' : ''" class="page-item">
+        <a @click.prevent="getTags(elem)" class="page-link" href="#">
+          {{ elem }}
+        </a>
+      </li>
+      <li class="page-item">
+        <a  @click.prevent="getTags(currentPage + 1) " class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 
 </template>
 
