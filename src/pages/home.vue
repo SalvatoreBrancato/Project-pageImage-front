bc458832a6f6b5d33292978ec9f4eac2c9ed8951
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-09-28 23:19:42 +0200

    date-visibility

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -58,25 +58,25 @@ data(){
 
 <template>
     <div>
-      <div v-for="(elem, index) in this.tags" :key="index">
+      <div v-for="(elem, index) in this.tags" :key="index" class="d-flex align-items-center">
         <input  type="checkbox" :value="elem.id" v-model="selectedTag">
-        <span>{{ elem.tag }}</span>
+        <span class="ms-2">{{ elem.tag }}</span>
       </div>
       <div>
         <input type="number" placeholder="inserisci quante foto visualizzare" v-model="numPerPage" min="3">
-        <button class="btn btn-primary" @click="this.getTags(this.numPerPage)">Submit</button>
+        <button class="btn btn-primary ms-2" @click="this.getTags(this.numPerPage)">Submit</button>
       </div>
     </div>
     <div class="container">
         <div class="row">
             <div id="card" class="col-12 col-md-6 col-lg-4"  v-for="(elem, index) in this.result" :key="index">
                 <router-link :to="{name:'singleImage', params:{id:elem.id}}">
-                    <img :src="`http://127.0.0.1:8000/storage/${elem.image}`" alt="">
+                    <img :src="`http://127.0.0.1:8000/storage/${elem.image}`" v-if="elem.visibility === 1" :class="d-none" alt="">
                 </router-link>
             </div>
         </div>
     </div>
-    <nav aria-label="Page navigation example">
+    <nav aria-label="Page navigation example" class="d-flex justify-content-center mt-5">
     <ul class="pagination">
       <li class="page-item">
         <a @click.prevent="getTags(currentPage - 1) " class="page-link" href="#" aria-label="Previous">
@@ -106,9 +106,12 @@ data(){
                 z-index: 99;
                 img{
                     width: 100%;
+                    height: 100%;
                     display: block;
                     object-fit: cover;
                     margin-top: 20px;
+                    border-radius: 20px 10px 20px 10px;
+                    box-shadow: 15px 15px 7px -5px rgba(0, 0, 0, 0.75);
                     &:hover{
                         transform: scale(105%);
                         transition: 800ms linear;
--- src/pages/singleImage.vue
+++ src/pages/singleImage.vue
@@ -15,7 +15,9 @@ data(){
         comment: '' ,
         visibility: '',
         image_id: '' 
-      }
+      },
+      //date: new Date(this.result.image.created_at),
+      //formattedDate : `${this.date.getDate()}/${this.date.getMonth() + 1}/${this.date.getFullYear()}`
     }
   },
   mounted(){
@@ -24,7 +26,6 @@ data(){
   },
   created(){
     this.chiamataApi();
-    
   },
   methods: {
     chiamataApi(){
@@ -43,6 +44,11 @@ data(){
              }
              this.chiamataApi();
         })
+    },
+    modificaData(data){
+      const date = new Date(data);
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      return date.toLocaleDateString('it-IT', options);
     }
   }
 }  
@@ -52,29 +58,30 @@ data(){
 
 <div class="container">
     <div class="row">
-        <div class="col-12 col-md-8 ">
+        <div class="col-12 col-md-8 animate__animated animate__backInLeft animate__slow">
             <img :src="`http://127.0.0.1:8000/storage/${result.image.image}`" alt="">
         </div>
         <div class="col-4">
-            <div>
+            <div class="animate__animated animate__backInRight animate__delay-2s">
                 <h1 class="text-capitalize">
                     {{ this.result.image.user.name}}
                 </h1>
                 <h2>
-                    {{ this.result.image.created_at }}
+                    {{ this.modificaData(this.result.image.created_at) }}
                 </h2>
                 <h3 v-for="(elem, index) in this.result.image.tags" :key="index">
-                    {{ elem.tag }}
+                    {{'#' + elem.tag }}
                 </h3>
             </div>
         </div>
     </div>
-
-    <form @submit.prevent="sendForm()">
+    <h2 v-if="(this.result.comments.length == 0)" class="mt-3">Commenta per primo</h2>
+    <h2 v-else class="mt-3">Lascia il tuo commento</h2>
+    <form class="mt-5" @submit.prevent="sendForm()">
         <input type="hidden" name="image_id">
         <input class="w-100 mt-1" type="text" name="comment" v-model="this.data.comment">
         <input type="hidden" name="visibility">            
-        <button class="btn btn-primary" type="submit">Submit</button>
+        <button class="btn btn-primary my-3" type="submit">Submit</button>
     </form>
     <div v-if="(this.result.comments.length == 0)">Non ci sono commenti</div>
     <div class="border" v-else v-for="(elem, index) in this.result.comments" :key="index">{{ elem.comment }}</div>
@@ -86,11 +93,13 @@ data(){
 <style lang="scss" scoped>
 
     img{
-        width: 100%;
-        height: 75vh;
+        max-width: 100%;
+        max-height: 75vh;
         display: block;
         object-fit: contain;
         margin-top: 20px;
+        border-radius: 20px 10px 20px 10px;
+        box-shadow: 15px 15px 7px -5px rgba(0, 0, 0, 0.75);
     }
 
 </style>
